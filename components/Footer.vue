e90ec6c640a08cd016ccc18eae87fd82b06e4975
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-05-18 19:02:58 +0800

    Progress: Add footer component

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <footer class="footer d-flex justify-content-center align-items-center text-center">
+      <p>© 2022 PT. Timedoor Indonesia. All rights reserved.</p>
+    </footer>
+  </div>
+</template>
