4abc4453ed235bab700642f1fe3954376754ef9a
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-05-18 19:04:28 +0800

    Progress: Add user story index pages

---
+++ pages/user/story/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <section class="user">
+      <div class="row justify-content-center">
+        <SidebarUser />
+        <div class="col-6">
+          <div class="card-user">
+            <h5 class="header-title mb-0">Story List</h5>
+            <div class="wrapper-body">
+              <table class="table table-striped table-bordered">
+                <thead>
+                  <tr>
+                    <td>Title</td>
+                    <td>Last Update</td>
+                    <td>Action</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Lorem Ipsum</td>
+                    <td>13:46, 17 May 2024</td>
+                    <td>Edit Button</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
