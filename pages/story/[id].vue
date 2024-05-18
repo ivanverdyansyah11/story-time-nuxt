66387d626e518a77c39c35d39c99d98eb2d8ce5d
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-05-18 19:03:56 +0800

    Progress: Add story detail pages

---
+++ pages/story/[id].vue
@@ -0,0 +1,50 @@
+<script setup lang="js">
+import { useRoute } from 'vue-router'
+import { useStoryStore } from '~/stores/story'
+import { formatDateTime } from '~/helpers/formatDate.ts'
+import { imageUrl } from "~/helpers/globalVariable";
+const routes = useRoute();
+const story = useStoryStore();
+story.getStoryById(routes.params.id)
+const storyDetail = story.$state.story;
+</script>
+
+<template>
+  <div class="mt-5 pt-5">
+    <section class="detail-story">
+      <div class="row">
+        <div class="col-lg-8 mb-4 mb-lg-0">
+          <h2 class="title">{{ storyDetail.title }}</h2>
+          <p class="date">{{ formatDateTime(storyDetail.createdAt) }}</p>
+          <img
+            v-if="storyDetail.cover_image.url"
+            :src="imageUrl + storyDetail.cover_image.url"
+            class="thumbnail-story"
+            alt="Story Image"
+          />
+          <div class="story-detail d-flex flex-column gap-2">
+            <p v-html="storyDetail.content"></p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div
+            class="card-author d-flex justify-content-center align-items-center flex-column"
+          >
+            <h6 class="author-caption">Author</h6>
+            <img
+              src="~/assets/images/story/story-sample.jpg"
+              width="70"
+              height="70"
+              alt="Author Image"
+              style="object-fit: cover"
+            />
+            <h6 class="author-name">{{ storyDetail.author.name }}</h6>
+            <p class="author-caption">
+              My name is {{ storyDetail.author.name }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
